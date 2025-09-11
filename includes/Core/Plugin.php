ba2db9fecb70ca188111d34474ac1cf5cb30a96c
Flav <131701096+FlavioSerra-afk@users.noreply.github.com>
2025-09-11 15:36:14 +0100

    chore(release): configure release-automation

--- includes/Core/Plugin.php
+++ includes/Core/Plugin.php
@@ -20,7 +20,7 @@ use FoodBankManager\Core\Screen;
 
 final class Plugin {
 
-    public const VERSION = '1.4.0-rc.7.7';
+    public const VERSION = '1.4.0-rc.7.7'; // x-release-please-version
 
         private static ?Plugin $instance = null;
         private static bool $booted = false;
