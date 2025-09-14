e1fd2b6b311bfde533debec106bc5c45640b48f6
Flav <131701096+FlavioSerra-afk@users.noreply.github.com>
2025-09-14 20:26:12 +0100

    test(meta): add WP admin stubs and sync release config

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,7 +4,9 @@ require_once __DIR__ . '/Support/Exceptions.php';
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/Support/BaseTestCase.php';
 require_once __DIR__ . '/Support/WPDBStub.php';
+require_once __DIR__ . '/../includes/Core/Plugin.php';
 require_once __DIR__ . '/../includes/Attendance/AttendanceRepo.php';
+require_once __DIR__ . '/Helpers/WPStubs.php';
 
 fbm_test_reset_globals();
 if (!defined('FBM_TESTS')) {
